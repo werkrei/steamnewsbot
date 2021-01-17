956bdd2f360d0c204f107bcf79c37d7c49732faa
werkrei <33633356+werkrei@users.noreply.github.com>
2021-01-17 16:40:25 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    accountName: 'clamorsnews',
-    password: '88Hane88',
-    sharedSecret: 'nPr1AFKoiGSYgkE2zR7f/BEbbkg=',
-    consumer_key:'w29Btni66hONFOy2dz1vKufdW',
-    consumer_secret:'6XYff54rYBBMZkKtzkUbIeUU7UbYYeQkt2iyghvr3peMnXZSiu',
-    access_token:'1069421270-tpFB1j25o52f9wMrvTrmRnx5G1WXk5IFdzMTs0j',
-    access_token_secret:'Mp4IuKH9Q4jP9dLU5RWQs5Bu9YdeoPwNlUxP67n92OYwU'
-}
